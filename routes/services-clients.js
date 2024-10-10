7e9e49ff83eb12792b8cb2372c14036cfc7a3cfc
juanmontrasi <juanmontrasi@gmail.com>
2024-10-10 10:05:28 -0300

    cambios servicios-clientes

--- routes/services-clients.js
+++ routes/services-clients.js
@@ -1,17 +1,17 @@
 import { Router } from "express"
 import { ServicesClientsController } from "../controllers/services-clients.js"
-// import { validateToken } from "../middlewares/token.js"
+import { validateToken } from "../middlewares/token.js"
 // Cuando tenga el front tengo que poner de segundo parametro validateToken
 
 export const createServicesClientsRouter = ({ servicesClientsModel }) => {
   const servicesClientsRouter = Router()
   const servicesClientsController = new ServicesClientsController({ servicesClientsModel })
 
-  servicesClientsRouter.get("/", servicesClientsController.getAll)
-  servicesClientsRouter.get("/:id_servicio/:id_usuario/:fecha_servicio", servicesClientsController.getById)
-  servicesClientsRouter.post("/", servicesClientsController.create)
-  servicesClientsRouter.delete("/:id_servicio/:id_usuario/:fecha_servicio", servicesClientsController.delete)
-  servicesClientsRouter.patch("/:id_servicio/:id_usuario/:fecha_servicio", servicesClientsController.update)
+  servicesClientsRouter.get("/", validateToken, servicesClientsController.getAll)
+  servicesClientsRouter.get("/:id_servicio/:id_usuario/:fecha_servicio", validateToken, servicesClientsController.getById)
+  servicesClientsRouter.post("/", validateToken, servicesClientsController.create)
+  servicesClientsRouter.delete("/:id_servicio/:id_usuario/:fecha_servicio", validateToken, servicesClientsController.delete)
+  servicesClientsRouter.patch("/:id_servicio/:id_usuario/:fecha_servicio", validateToken, servicesClientsController.update)
 
 
   return servicesClientsRouter
