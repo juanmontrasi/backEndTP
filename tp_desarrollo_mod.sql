ed295ac0a65a304bf1c9ce3233a3b1ccc1622b26
juanmontrasi <juanmontrasi@gmail.com>
2025-02-04 12:09:07 -0300

    Cambios base de datos

--- tp_desarrollo_mod.sql
+++ tp_desarrollo_mod.sql
@@ -1,9 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `tp_desarrollo_mod` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `tp_desarrollo_mod`;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
 -- Host: localhost    Database: tp_desarrollo_mod
 -- ------------------------------------------------------
--- Server version	8.4.0
-
+-- Server version	8.0.40
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,37 +17,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE database tp_desarrollo_mod; 
-use tp_desarrollo_mod;
-
---
--- Table structure for table `clientes_servicios`
---
-
-DROP TABLE IF EXISTS `clientes_servicios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `clientes_servicios` (
-  `id_servicio` int NOT NULL,
-  `id_usuario` int NOT NULL AUTO_INCREMENT,
-  `fecha_servicio` datetime NOT NULL,
-  `mensaje` varchar(45) DEFAULT NULL,
-  KEY `id_servicio_idx` (`id_servicio`),
-  KEY `id_usuario_idx` (`id_usuario`),
-  CONSTRAINT `fk_clientes_servicios_id_servicios` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicios`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuarios`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `clientes_servicios`
---
-
-LOCK TABLES `clientes_servicios` WRITE;
-/*!40000 ALTER TABLE `clientes_servicios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `clientes_servicios` ENABLE KEYS */;
-UNLOCK TABLES;
-
 --
 -- Table structure for table `pedidos`
 --
@@ -57,11 +27,13 @@ DROP TABLE IF EXISTS `pedidos`;
 CREATE TABLE `pedidos` (
   `id_pedidos` int NOT NULL AUTO_INCREMENT,
   `fecha_pedido` datetime NOT NULL,
-  `total` float NOT NULL,
+  `total` float DEFAULT NULL,
   `id_cliente` int NOT NULL,
+  `estado` varchar(255) NOT NULL,
   PRIMARY KEY (`id_pedidos`),
-  KEY `id_cliente_idx` (`id_cliente`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `id_cliente_idx` (`id_cliente`),
+  CONSTRAINT `fk_usuarios_id_usuarios` FOREIGN KEY (`id_cliente`) REFERENCES `usuarios` (`id_usuarios`)
+) ENGINE=InnoDB AUTO_INCREMENT=290 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +42,6 @@ CREATE TABLE `pedidos` (
 
 LOCK TABLES `pedidos` WRITE;
 /*!40000 ALTER TABLE `pedidos` DISABLE KEYS */;
-INSERT INTO `pedidos` VALUES (1,'2024-09-24 19:32:50',1000,1),(2,'2024-09-24 19:34:02',1000,1);
 /*!40000 ALTER TABLE `pedidos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,12 +55,13 @@ DROP TABLE IF EXISTS `productos`;
 CREATE TABLE `productos` (
   `id_productos` int NOT NULL AUTO_INCREMENT,
   `nombre_producto` varchar(45) NOT NULL,
-  `desc_producto` varchar(45) NOT NULL,
+  `desc_producto` varchar(255) NOT NULL,
   `stock` int NOT NULL,
   `precio` float NOT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_productos`),
   UNIQUE KEY `id_productos_UNIQUE` (`id_productos`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,6 +70,7 @@ CREATE TABLE `productos` (
 
 LOCK TABLES `productos` WRITE;
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (1,'Grafica Nvidia RTX 4060 Ti','La NVIDIA RTX 4060 Ti, con 8 GB o 16 GB de GDDR6, es ideal para gaming en 1440p y tareas creativas. Incluye Ray Tracing y DLSS 3, ofreciendo gráficos realistas y alto rendimiento con eficiencia energética',75,800,'grafica-nvidia.jpg'),(2,'Acer Nitro V 15.6\"','La Acer Nitro V 15.6\" es una laptop para gaming con pantalla Full HD, diseño potente y rendimiento optimizado. Ideal para juegos y multitarea avanzada',12,1500,'laptop-gamer.jpg'),(3,'Auriculares JBL Quantum 910','Los auriculares JBL Quantum 910 ofrecen sonido envolvente, cancelación de ruido activa y micrófono de alta calidad. Ideales para gaming inmersivo y comunicación clara.',56,299,'auriculares.jpg'),(4,'Memoria Team DDR5 32GB','La memoria Team DDR5 32GB ofrece alto rendimiento y velocidad mejorada para tareas exigentes, ideal para gaming, creación de contenido y sistemas de última generación.',771,899,'memoria-ram.jpg'),(6,'Monitor 4K Curvo','El Monitor 4K Curvo ofrece una experiencia inmersiva con resolución UHD y diseño panorámico. Ideal para gaming, trabajo y entretenimiento con imágenes nítidas y detalladas.',99,349,'monitor.jpg'),(7,'Teclado Mecanico','El teclado mecánico ofrece alta precisión y durabilidad, con switches táctiles ideales para gaming y escritura. Combina respuesta rápida con diseño ergonómico.',99,500,'teclado-mecanico.jpg');
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -113,8 +86,10 @@ CREATE TABLE `productos_pedidos` (
   `id_producto` int NOT NULL,
   `cantidad` int NOT NULL,
   `subtotal` int NOT NULL,
+  PRIMARY KEY (`id_pedidos`,`id_producto`),
   KEY `id_pedido_idx` (`id_pedidos`),
-  KEY `id_producto_idx` (`id_producto`),
+  KEY `fk_productos_id_productos_idx` (`id_producto`),
+  CONSTRAINT `fk_productos_id_productos` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_productos`),
   CONSTRAINT `fk_productos_pedidos_id_pedidos` FOREIGN KEY (`id_pedidos`) REFERENCES `pedidos` (`id_pedidos`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -126,30 +101,6 @@ CREATE TABLE `productos_pedidos` (
 LOCK TABLES `productos_pedidos` WRITE;
 /*!40000 ALTER TABLE `productos_pedidos` DISABLE KEYS */;
 /*!40000 ALTER TABLE `productos_pedidos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `servicios`
---
-
-DROP TABLE IF EXISTS `servicios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `servicios` (
-  `id_servicios` int NOT NULL AUTO_INCREMENT,
-  `desc_servicio` varchar(45) NOT NULL,
-  `precio` float NOT NULL,
-  PRIMARY KEY (`id_servicios`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `servicios`
---
-
-LOCK TABLES `servicios` WRITE;
-/*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -170,8 +121,10 @@ CREATE TABLE `usuarios` (
   `apellido` varchar(45) NOT NULL,
   `direccion` varchar(45) NOT NULL,
   PRIMARY KEY (`id_usuarios`),
-  UNIQUE KEY `id_usuarios_UNIQUE` (`id_usuarios`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `id_usuarios_UNIQUE` (`id_usuarios`),
+  UNIQUE KEY `nombre_usuario_UNIQUE` (`nombre_usuario`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=107 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -179,6 +132,9 @@ CREATE TABLE `usuarios` (
 --
 
 LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (67,'admin','admin',1,'admin@admin.com','1111111111','admin','admin','admin');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -190,4 +146,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-24 16:53:22
+-- Dump completed on 2025-02-04 12:08:03
