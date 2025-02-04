4b4b119d0fe0f0d643241a9ba6726918e35414b3
juanmontrasi <juanmontrasi@gmail.com>
2025-02-04 12:23:27 -0300

    cambios en el archivo .env

--- models/sequelize.js
+++ models/sequelize.js
@@ -2,6 +2,11 @@ import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv';
 dotenv.config();
 
+if (!process.env.BDPASSWORD) {
+  console.error("ERROR: La variable de entorno BDPASSWORD no está definida.");
+  process.exit(1);
+}
+
 const PASSWORD = process.env.BDPASSWORD
 const sequelize = new Sequelize('tp_desarrollo_mod', 'root', PASSWORD, {
   host: 'localhost',
